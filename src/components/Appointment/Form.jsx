cffd5da1c855c34e7ea92f4e9e64737c416c50de
Jeremy <jeremy.nasato12@gmail.com>
2020-05-12 16:10:32 -0700

    create component and stories for appointment form

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -5,8 +5,22 @@ import Button from "../Button";
 
 export default function Form(props) {
 
-  const [name, setName] = useState("");
-  const [interviewer, setInterviewer] = useState(0);
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  }
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+
+  const save = () => {
+    props.onSave(name, interviewer)
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -18,14 +32,15 @@ export default function Form(props) {
             type="text"
             placeholder="Enter Student Name"
             onChange={(event) => setName(event.target.value)}
+            value={name}
           />
         </form>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={save}>Save</Button>
         </section>
       </section>
     </main>
