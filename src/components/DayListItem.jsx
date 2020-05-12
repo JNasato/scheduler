2dd025cb5af06a5e0f8ec4ce48e2d55684a99eb5
Jeremy <jeremy.nasato12@gmail.com>
2020-05-12 15:44:45 -0700

    create components for day list, interviewer list, as well as the appointment sub-components

---
+++ src/components/DayListItem.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+    const dayClass = classNames("day-list__item", {
+      "day-list__item--selected": props.selected,
+      "day-list__item--full": props.spots === 0
+    })
+
+    const formatSpots = function(props) {
+      switch(props.spots) {
+        case 0:
+          return "no spots remaining";
+        case 1:
+          return "1 spot remaining";
+        default: 
+          return `${props.spots} spots remaining`;
+      }
+    }
+
+    return (
+    <li className={dayClass} onClick={props.setDay}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props)}</h3>
+    </li>
+  );
+}
