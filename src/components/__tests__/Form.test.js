1606f233e962e7fedd79048cf90ce7a775dbdc29
Jeremy <jeremy.nasato12@gmail.com>
2020-05-20 16:39:11 -0700

    fix broken test in form and general code cleanup

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -36,7 +36,7 @@ describe("Form", () => {
 
   it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
-    const { getByText, getByPlaceholderText, queryByText } = render(
+    const { getByText, getByPlaceholderText, getByAltText, queryByText } = render(
       <Form interviewers={interviewers} onSave={onSave} />
     );
 
@@ -52,9 +52,14 @@ describe("Form", () => {
     fireEvent.click(getByText("Save"));
 
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
+    expect(getByText(/interviewer must be chosen/i)).toBeInTheDocument();
 
+    fireEvent.click(getByAltText("Sylvia Palmer"));
+    fireEvent.click(getByText("Save"));
+
+    expect(queryByText(/interviewer must be chosen/i)).toBeNull();
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
 
   it("calls onCancel and resets the input field", () => {
